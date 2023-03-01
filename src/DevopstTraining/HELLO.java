fd7f0ba5ead2e9a2cfa57e89b82b95a9d4130ea0
veenasridhar <veenasridhar@localhost>
2023-02-28 16:05:27 -0800

    uploading my project

---
+++ src/DevopstTraining/HELLO.java
@@ -0,0 +1,16 @@
+package DevopstTraining;
+
+public class HELLO {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to the Java world");
+		
+System.out.println(10+30);
+System.out.println("hello good morning");
+
+
+System.out.println("Training for DevOps");
+System.out.println("$100 USD discount on iphone 14");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author veenasridhar
+ *
+ */
+module myfirstproject {
+}
