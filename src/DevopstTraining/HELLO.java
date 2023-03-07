9cc774b972e2c9a66a7287e757bf2cd762b5650a
veenasridhar <veenasridhar@localhost>
2023-03-07 15:28:15 -0800

    changes made to how are you

--- src/DevopstTraining/HELLO.java
+++ src/DevopstTraining/HELLO.java
@@ -12,7 +12,7 @@ System.out.println("hello good morning");
 System.out.println("Training for DevOps");
 System.out.println("$100 USD discount on iphone 14");
 	System.out.println("testingwebhook for the first time");
-	System.out.println("how are you");
+	System.out.println("how ");
 	
 	}
 	
