89a744e6d94cbe58320dc540b85a2838856c9cd4
veenasridhar <veenasridhar@localhost>
2023-03-08 14:36:34 -0800

    commit the code

--- src/DevopstTraining/HELLO.java
+++ src/DevopstTraining/HELLO.java
@@ -13,6 +13,7 @@ System.out.println("Training for DevOps");
 System.out.println("$100 USD discount on iphone 14");
 	System.out.println("testingwebhook for the first time");
 	System.out.println("how ");
+	System.out.println("today top deal");
 	
 	}
 	
